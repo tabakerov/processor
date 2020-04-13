13ae8504a5f38e2ade0ac241f69e1d46f53ee93f
Dmitry Tabakerov <tabakerov@outlook.com>
2020-04-14 00:00:03 +0300

    update scratch file with new ops

--- Processor/scratches.fsx
+++ Processor/scratches.fsx
@@ -1,4 +1,4 @@
-type CPU = {IP: int; A: int; B: int }
+type CPU = {IP: int; A: int; B: int; CRJ: int }
 
 let rawram = [|1; 2; 2; 3; 4|]
 let ram = rawram
@@ -12,6 +12,14 @@ let ``process`` cpu (ram : int[])  =
         { cpu with IP = cpu.IP + 2; A = ram.[cpu.IP+1] }
     | 2 -> // add next memory item to A
         { cpu with IP = cpu.IP + 2; A = cpu.A + ram.[cpu.IP+1] }
+    | 3 -> // check if A > B and write CJR to IP if true
+        match cpu.A > cpu.B with
+        | true -> { cpu with IP = cpu.CRJ }
+        | false -> { cpu with IP = cpu.IP + 1 }
+    | 4 -> // check if A = B and write CJR to IP if true
+        match cpu.A = cpu.B with
+        | true -> { cpu with IP = cpu.CRJ }
+        | false -> { cpu with IP = cpu.IP + 1 }
     | _ -> cpu
         
 let cpu = { IP = 0; A = 0; B = 0; }
