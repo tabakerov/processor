2d5ede0b37c67bc614a36c2b57cc883459908d7a
Dmitry Tabakerov <tabakerov@outlook.com>
2020-04-14 15:24:13 +0300

    add memory read/write register, add read memory operation

--- Processor/cpu.fs
+++ Processor/cpu.fs
@@ -11,6 +11,7 @@ module CPU
         IP : int    // instruction pointer
         A  : int    // general purpose register
         B  : int    // general purpose register
+        RWA: int    // register for memory address to read/write
         JP : int    // unconditional jump pointer
         CJR: int    // conditional jump pointer
     }
@@ -23,18 +24,20 @@ module CPU
         | LoadA = 1
         | LoadB = 2
         | SwapAB = 3
-        | Write = 4
+        | SetRWA = 4
+        | Read  = 5
+        | Write = 6
 
         // ARITHMETIC
-        | AddAB = 5
+        | AddAB = 7
 
         // COMPARISON/CONDITIONS
-        | AGreaterB = 6
-        | ABEquals = 7
-        | SetCJR = 8
+        | AGreaterB = 8
+        | ABEquals = 9
+        | SetCJR = 10
         
-        | SetJP = 9
-        | Jump = 10
+        | SetJP = 11
+        | Jump = 12
 
     type Ops =
         | Halt
@@ -44,6 +47,8 @@ module CPU
         | LoadA
         | LoadB
         | SwapAB
+        | SetRWA
+        | Read
         | Write
      
         // ARITHMETIC
@@ -64,6 +69,8 @@ module CPU
         // MEMORY READ-WRITE
         | OpsEnum.LoadA -> LoadA
         | OpsEnum.LoadB -> LoadB
+        | OpsEnum.SetRWA -> SetRWA
+        | OpsEnum.Read -> Read
         | OpsEnum.Write -> Write
         | OpsEnum.SwapAB -> SwapAB
 
@@ -84,6 +91,8 @@ module CPU
         | Noop -> int OpsEnum.Noop
         | LoadA -> int OpsEnum.LoadA
         | LoadB -> int OpsEnum.LoadB
+        | SetRWA -> int OpsEnum.SetRWA
+        | Read -> int OpsEnum.Read
         | Write -> int OpsEnum.Write
         | SwapAB -> int OpsEnum.SwapAB
         | AddAB -> int OpsEnum.AddAB
@@ -101,6 +110,8 @@ module CPU
             | CpuMode.Halt -> (cpu, ram)
             | CpuMode.Run ->
                 let instruction = ram.[cpu.IP] |> decodeInstruction
+                printfn "Instruction: %A, CPU: %A" instruction cpu
+                Console.ReadKey() |> ignore
                 match instruction with
                 | Halt -> // this is the end
                     ``process`` { cpu with Mode = CpuMode.Halt} ram
@@ -110,8 +121,12 @@ module CPU
                     ``process`` { cpu with IP = cpu.IP + 2; A = ram.[cpu.IP+1] } ram
                 | LoadB -> // load next memory item to B, IP jumps over
                     ``process`` { cpu with IP = cpu.IP + 2; B = ram.[cpu.IP+1] } ram
-                | Write -> // write A to memory with address B, IP + 1
-                    ram.[cpu.B] <- cpu.A
+                | SetRWA -> // load next memory item to RWA, IP jumps over
+                    ``process`` { cpu with IP = cpu.IP + 2; RWA = ram.[cpu.IP+1] } ram
+                | Read -> // load from RWA memory address to A, IP + 1
+                    ``process`` { cpu with IP = cpu.IP + 1; A = ram.[cpu.RWA]}
+                | Write -> // write A to memory with address RWA, IP + 1
+                    ram.[cpu.RWA] <- cpu.A
                     ``process`` {cpu with IP = cpu.IP + 1} ram
                 | SwapAB -> // A <-> B, IP + 1
                     ``process`` { cpu with IP = cpu.IP + 1; A = cpu.B; B = cpu.A } ram
